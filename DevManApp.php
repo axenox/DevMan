f407875b09fa8d24d2590c622b9e4917ab251ce5
Andrej Kabachnik <andrej_ka@gmx.de>
2017-05-25 10:09:13 +0200

    REFACTOR code style now compatible with PSR-12

--- DevManApp.php
+++ DevManApp.php
@@ -1,17 +1,24 @@
-<?php namespace axenox\DevMan;
-				
+<?php
+
+namespace axenox\DevMan;
+
 use exface\Core\Interfaces\InstallerInterface;
 use exface\SqlDataConnector\SqlSchemaInstaller;
 
-class DevManApp extends \exface\Core\CommonLogic\AbstractApp {
-	
-	public function get_installer(InstallerInterface $injected_installer = null){
-		$installer = parent::get_installer($injected_installer);
-		
-		$schema_installer = new SqlSchemaInstaller($this->get_name_resolver());
-		$schema_installer->set_data_connection($this->get_workbench()->model()->get_object('axenox.DevMan.topic')->get_data_connection());
-		$installer->add_installer($schema_installer);
-		
-		return $installer;
-	}
+class DevManApp extends \exface\Core\CommonLogic\AbstractApp
+{
+
+    public function getInstaller(InstallerInterface $injected_installer = null)
+    {
+        $installer = parent::getInstaller($injected_installer);
+        
+        $schema_installer = new SqlSchemaInstaller($this->getNameResolver());
+        $schema_installer->setDataConnection($this->getWorkbench()
+            ->model()
+            ->getObject('axenox.DevMan.topic')
+            ->getDataConnection());
+        $installer->addInstaller($schema_installer);
+        
+        return $installer;
+    }
 }	

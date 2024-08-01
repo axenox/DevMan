05212e46b42259d1a1437f7c774a2e37f4071362
Andrej Kabachnik <kabachello@users.noreply.github.com>
2024-08-01 12:58:49 +0200

    FIX SQL Migratino for test plan comments

--- Install/Sql/MySQL/Migrations/3.01/20240228_1330_1_NEW_test_plan_cases_comment.sql
+++ Install/Sql/MySQL/Migrations/3.01/20240228_1330_1_NEW_test_plan_cases_comment.sql
@@ -1,11 +1,7 @@
 -- UP
-IF COL_LENGTH('test_plan_cases', `comment`) IS NULL
 ALTER TABLE test_plan_cases
-ADD `comment` varchar(1000) NULL
-GO;
+ADD COLUMN `comment` varchar(1000) NULL
 
 -- DOWN
-IF COL_LENGTH('test_plan_cases', `comment`) IS NOT NULL
 ALTER TABLE test_plan_cases
 DROP COLUMN `comment`
-GO;
